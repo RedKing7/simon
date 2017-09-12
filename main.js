69c5d401ca0321ce1426620f98966ffe7bf20a8e
Rory Johnson <redkjohnson@gmail.com>
2017-09-12 10:17:27 -0400

    assigned consts to color/numbers

--- main.js
+++ main.js
@@ -1,98 +1,124 @@
 const SPEED = 2000; //2 seconds
 
-const playersTurn = (colors) =>{
-    console.log('PLAYERS TURN');
-    $('#green').on({
-        'mouseover': function(event){
-            $(event.target).css('background-color', 'green')
-        },
-        'mouseleave': function(event){
-            $(event.target).css('background-color', 'DarkGreen')            
-        },
-        'click': function(event){
-            $(event.target).css('background-color', 'lime')
-        }
-    })
+const GREEN = 0;
+const RED = 1;
+const YELLOW = 2;
+const BLUE = 3;
 
-    $('#red').on({
-        'mouseover': function(event){
-            $(event.target).css('background-color', 'firebrick')            
-        },
-        'mouseleave': function(event){
-            $(event.target).css('background-color', 'darkred')
-        },
-        'click': function(event){
-            $(event.target).css('background-color', 'red')            
-        }
-    })
 
-    $('#yellow').on({
-        'mouseover': function(event){
-            $(event.target).css('background-color', 'goldenrod')            
-        },
-        'mouseleave': function(event){
-            $(event.target).css('background-color', 'darkgoldenrod')            
-        },
-        'click': function(event){
-            $(event.target).css('background-color', 'yellow')
-        }
-    })
-
-    $('#blue').on({
-        'mouseover': function(event){
-            $(event.target).css('background-color', 'mediumblue')            
-        },
-        'mouseleave': function(event){
-            $(event.target).css('background-color', 'darkblue')            
-        },
-        'click': function(event){
-            $(event.target).css('background-color', 'blue')
-        }
-    })
-
-    updateScore(colors);
-}
-
-const computersTurn = (colors) =>{
-    console.log('COMPUTERS TURN');
+const listenersOff = () =>{
     $('#green').off();
     $('#red').off();
     $('#yellow').off();
     $('#blue').off();
+}
+
+const playersTurn = (colors) =>{
+    console.log('PLAYERS TURN');
+    let guess = [];
+    $('#green').on({
+        'mouseover': function(event){
+            $(event.target).css('background-color', 'green');
+        },
+        'mouseleave': function(event){
+            $(event.target).css('background-color', 'DarkGreen');         
+        },
+        'click': function(event){
+            $(event.target).css('background-color', 'lime');
+
+        }
+    });
+
+    $('#red').on({
+        'mouseover': function(event){
+            $(event.target).css('background-color', 'firebrick');        
+        },
+        'mouseleave': function(event){
+            $(event.target).css('background-color', 'darkred');
+        },
+        'click': function(event){
+            $(event.target).css('background-color', 'red');
+
+        }
+    });
+
+    $('#yellow').on({
+        'mouseover': function(event){
+            $(event.target).css('background-color', 'goldenrod');           
+        },
+        'mouseleave': function(event){
+            $(event.target).css('background-color', 'darkgoldenrod');           
+        },
+        'click': function(event){
+            $(event.target).css('background-color', 'yellow');
+
+        }
+    });
+
+    $('#blue').on({
+        'mouseover': function(event){
+            $(event.target).css('background-color', 'mediumblue');            
+        },
+        'mouseleave': function(event){
+            $(event.target).css('background-color', 'darkblue');            
+        },
+        'click': function(event){
+            $(event.target).css('background-color', 'blue');
+
+        }
+    });
+
+    do{
+
+    }while(guess.length < colors.length);
+
+    listenersOff();
+
+    if(guess === colors){
+        updateScore(colors);
+        computersTurn(colors); 
+    } else { 
+        gameOver();
+    }
+}
+
+const computersTurn = (colors) =>{
+    console.log('COMPUTERS TURN');
+    listenersOff();
 
     let newColor = Math.floor(Math.random() * 4);
     colors.push(newColor);
 
     lightSequence(colors);
-    //playersTurn(colors);
+    playersTurn(colors);
 }
 
 const lightSequence = (colors) =>{
     console.log('LIGHT SEQUENCE');
     colors.forEach(function(color) {
         switch(color){
-            case 0:
+            case GREEN:
                 $('#green').css('background', 'lime');
                 setTimeout(function(){
                     $('#green').css('background', 'darkgreen');
                 }, SPEED);
                 break;
 
-            case 1:
+            case RED:
                 $('#red').css('background', 'red');
                 setTimeout(function(){
                     $('#red').css('background', 'darkred');                    
                 }, SPEED);
                 break;
 
-            case 2:
+            case YELLOW:
                 $('#yellow').css('background', 'yellow');
                 setTimeout(function(){
                     $('#yellow').css('background', 'darkgoldenrod');                    
                 }, SPEED);
                 break;
 
-            case 3:
+            case BLUE:
                 $('#blue').css('background', 'blue');
                 setTimeout(function(){
                     $('#blue').css('background', 'darkblue');                    
@@ -101,13 +127,14 @@ const lightSequence = (colors) =>{
 
             default:
                 alert('ERROR');
+                //this should never happen
         }
     });
 }
 
 const updateScore = (colors) =>{
     console.log('UPDATE SCORE');
-    $('#score').text(colors.length);
+    $('#score').text(`Score: ${colors.length}`);
 }
 
 const gameOver = () =>{
@@ -116,6 +143,7 @@ const gameOver = () =>{
 
 $('#display').on({
     'click': function(event){
+        $('#display').off();
         console.log('START');
         let colors = [];
         computersTurn(colors);
