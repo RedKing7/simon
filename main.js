dc368f4355c898c548261b921cd12f879fd5266a
Rory Johnson <redkjohnson@gmail.com>
2017-09-12 12:55:10 -0400

    restructured some code

--- main.js
+++ main.js
@@ -5,12 +5,10 @@ const RED = 1;
 const YELLOW = 2;
 const BLUE = 3;
 
-var sequence = [];
-var guess = [];
+var sequence;
+var guess;
 var running;
 var playersTurn;
-
-var guessIndex;
 
 $('.simon-button').on({
     'mouseover': function(event){
@@ -18,19 +16,19 @@ $('.simon-button').on({
             $button = $(event.target);
             switch($button.attr('id')){
                 case 'green':
-                    $button.css('background-color', 'green');
+                    $('#green').css('background-color', 'green');
                     break;
 
                 case 'red':
-                    $button.css('background-color', 'firebrick');
+                    $('#red').css('background-color', 'firebrick');
                     break;
 
                 case 'yellow':
-                    $button.css('background-color', 'goldenrod');
+                    $('#yellow').css('background-color', 'goldenrod');
                     break;
 
                 case 'blue':
-                    $button.css('background-color', 'mediumblue');
+                    $('#blue').css('background-color', 'mediumblue');
                     break;
 
                 default: alert("something broke");
@@ -43,19 +41,19 @@ $('.simon-button').on({
             $button = $(event.target);
             switch($button.attr('id')){
                 case 'green':
-                    $button.css('background-color', 'darkgreen');
+                    $('#green').css('background-color', 'darkgreen');
                     break;
 
                 case 'red':
-                    $button.css('background-color', 'darkred');
+                    $('#red').css('background-color', 'darkred');
                     break;
 
                 case 'yellow':
-                    $button.css('background-color', 'darkgoldenrod');
+                    $('#yellow').css('background-color', 'darkgoldenrod');
                     break;
 
                 case 'blue':
-                    $button.css('background-color', 'darkblue');
+                    $('#blue').css('background-color', 'darkblue');
                     break;
 
                 default: alert("something broke");
@@ -68,27 +66,27 @@ $('.simon-button').on({
             $button = $(event.target);
             switch($button.attr('id')){
                 case 'green':
-                    $button.css('background-color', 'lime');
+                    $('#green').css('background-color', 'lime');
                     guess.push(GREEN);
-                    setTimeout(function(){$button.css('background-color', 'darkgreen')}, 700);
+                    setTimeout(function(){$('#green').css('background-color', 'darkgreen')}, 200);
                     break;
 
                 case 'red':
-                    $button.css('background-color', 'red');
+                    $('#red').css('background-color', 'red');
                     guess.push(RED);
-                    setTimeout(function(){$button.css('background-color', 'darkred')}, 700);
+                    setTimeout(function(){$('#red').css('background-color', 'darkred')}, 200);
                     break;
 
                 case 'yellow':
-                    $button.css('background-color', 'yellow');
+                    $('#yellow').css('background-color', 'yellow');
                     guess.push(YELLOW);
-                    setTimeout(function(){$button.css('background-color', 'darkgoldenrod')}, 700);
+                    setTimeout(function(){$('#yellow').css('background-color', 'darkgoldenrod')}, 200);
                     break;
 
                 case 'blue':
-                    $button.css('background-color', 'blue');
+                    $('#blue').css('background-color', 'blue');
                     guess.push(BLUE);
-                    setTimeout(function(){$button.css('background-color', 'darkblue')}, 700);
+                    setTimeout(function(){$('#blue').css('background-color', 'darkblue')}, 200);
                     break;
 
                 default: alert("something broke"); //this should never happen
@@ -97,6 +95,7 @@ $('.simon-button').on({
             for(let i = 0; i < guess.length; i++){
                 if(guess[i] !== sequence[i]){
                     gameOver();
+                    return;
                 }
             }
 
@@ -120,6 +119,7 @@ const computersTurn = () =>{
 
     setTimeout(function(){ //wait 1 second before letting player click
         console.log('PLAYERS TURN');
+        guess = []; // reset guess array
         playersTurn = true;
     }, 1000);
 }
@@ -129,31 +129,39 @@ const lightSequence = () =>{
     sequence.forEach(function(color) {
         switch(color){
             case GREEN:
-                $('#green').css('background', 'lime');
+                setTimeout(function(){
+                    $('#green').css('background', 'lime');                    
+                }, 500);
                 setTimeout(function(){
                     $('#green').css('background', 'darkgreen');
-                }, SPEED);
+                }, 500);
                 break;
 
             case RED:
-                $('#red').css('background', 'red');
+                setTimeout(function(){
+                    $('#red').css('background', 'red');
+                }, 500);
                 setTimeout(function(){
                     $('#red').css('background', 'darkred');                    
-                }, SPEED);
+                }, 500);
                 break;
 
             case YELLOW:
-                $('#yellow').css('background', 'yellow');
+                setTimeout(function(){
+                    $('#yellow').css('background', 'yellow');
+                }, 500);
                 setTimeout(function(){
                     $('#yellow').css('background', 'darkgoldenrod');                    
-                }, SPEED);
+                }, 500);
                 break;
 
             case BLUE:
-                $('#blue').css('background', 'blue');
+                setTimeout(function(){    
+                    $('#blue').css('background', 'blue');
+                }, 500);
                 setTimeout(function(){
                     $('#blue').css('background', 'darkblue');                    
-                }, SPEED);
+                }, 500);
                 break;
 
             default:
@@ -170,14 +178,17 @@ const updateScore = () =>{
 
 const gameOver = () =>{
     console.log('GAME OVER');
-    alert('LOSE');
+    running = false;
 }
 
 $('#display').on({
     'click': function(event){
         if(!running){
             console.log('START');
+            sequence = [];
+            guess = [];
             running = true;
+            updateScore();
             setTimeout(function(){ // wait 1 second before starting
                 computersTurn();
             }, 1000);
